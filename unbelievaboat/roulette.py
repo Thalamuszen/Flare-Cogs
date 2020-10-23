b055ec6037be6a6814a794408e3189883d0f9e33
Thalamuszen <thalamuszen@hotmail.com>
2020-10-23 13:57:34 +0100

    Update roulette.py

--- unbelievaboat/roulette.py
+++ unbelievaboat/roulette.py
@@ -333,7 +333,7 @@ class Roulette(MixinMeta):
         if betret.get("failed") is not None:
             return await ctx.send(betret["failed"])
         await ctx.send(
-            f"{ctx.author.mention} has placed a {humanize_number(amount)} {await bank.get_currency_name(ctx.guild)} bet on {bet}."
+            f"{ctx.author} has placed a {humanize_number(amount)} {await bank.get_currency_name(ctx.guild)} bet on {bet}."
         )
 
     @roulette_disabled_check()
